c1d4242a3dd551262402856c8f9eb87446f4c1cb
Franciscone Luiz <francisconeluiz@gmail.com>
2019-04-25 21:50:45 -0300

    Comentarios aula & duvidas

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 # Adicionar a escolha do tempo de espera em passos ou se o usuario tem tempo para esperar achar a resposta
 # Mostra a escolha do melhor metodo
+# Distancia de manhatan ou a proximidade da solução (Ou os dois no caso da A*)
 
 import copy
 
